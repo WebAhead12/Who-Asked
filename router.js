56e709b68e7446b04f9ddfe49347bbd84757a3e2
Mohammad Faur <mohammad.fa@live.com>
2021-11-16 15:53:19 +0200

    built router and server..

---
+++ router.js
@@ -0,0 +1,23 @@
+const router = express.Router();
+const express = require('express');
+const handlers = require('./handler');
+
+
+router.get('/', handlers.home);
+router.post('/register', handlers.register);
+router.post('/login', handlers.login);
+router.get('/data/:user', handlers.getUserQuestions);
+router.post('/data/:user', handlers.setQuestionOrAnswer);
+router.get('/profile/:user', handlers.getProfile);//when log in
+router.get('/view/:user', handlers.getUser); //when search
+router.get('/image/:user', handlers.getImageId);
+router.post('/image/:user', handlers.setImageId);
+router.get('/logout', handlers.logout);
+router.use((req, res) => {//strange route => redirect to homepage
+    res.redirect('/');
+})
+
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = router('./router');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json())
+app.use(router);
+app.use(express.static('assets'));
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});
+
