3bdada5871cd90f2f30d61c7cc3a0431ed54575f
Nur1995 <79867892+Nur1995@users.noreply.github.com>
2021-11-16 15:50:57 +0200

    initiated handler.js, fixed Assets to assets, fixed stuff in server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = require("./router");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 app.use(router)
+app.use(express, static('assets'));
+app.use(express, static('public'));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
