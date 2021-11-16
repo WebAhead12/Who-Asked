2db1228c52c8e58412b56344c38cb1d77bd448d8
Mohammad Faur <mohammad.fa@live.com>
2021-11-16 21:20:12 +0200

    fixing insertion on post table in init.sql file

--- database/init.sql
+++ database/init.sql
@@ -6,25 +6,25 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(25) NOT NULL,
   password VARCHAR(25) NOT NULL,
-  imageId VARCHAR(10), 
+  imageId VARCHAR(10)
 );
 
 CREATE TABLE posts (
  id SERIAL PRIMARY KEY,
  question TEXT NOT NULL,
  answer TEXT, 
- date DATE NOT NULL 
  user_id INTEGER REFERENCES users(id),
+ date TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 INSERT INTO users (username, password, imageId)  VALUES
-  ('notJulio', '123', "")
-;
+  ('notJulio', '123', '');
 
-INSERT INTO posts (question, answer, date, user_id) VALUES
-  ('What is your name', 'Juan',  , 1),
-  ('How old are you', 1),
-  ('Who are you, really?', 1)
+INSERT INTO posts (question, answer, user_id) VALUES
+  ('What is your name', 'Juan' , 1),
+  ('How old are you', 'I`m 21 years old.', 1),
+  ('Who are you, really?', 'Oh, don`t ask!', 1),
+  ('Is there a question that you can`t answer?', '', 1);
 
 
 COMMIT;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = router('./router');
+const cookieParser = requier('cookie-parser')
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 app.use(express.static('assets'));
