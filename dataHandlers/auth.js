a9536e27ccd7375ff07707f0f4d0aee039d86669
Mohammad Faur <mohammad.fa@live.com>
2021-11-17 20:07:00 +0200

    login

--- dataHandlers/auth.js
+++ dataHandlers/auth.js
@@ -7,9 +7,9 @@ const SECRET = "lnvz2342dnv89nSDJkout345";
 function checkCredential(user, password) {
 
   return users.getUser(user).then(result => {
-    if (!result)
+    if (!result.length)
       return { response: 'NotFound' };
-    else if (result[0].password != password)
+    if (result[0].password != password)
       return { response: 'WrongPassword' };
     else
       return { response: 'Successful' };
