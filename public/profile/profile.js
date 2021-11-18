f84153147c070fe56e377382b4e056d61553eebb
Nur1995 <79867892+Nur1995@users.noreply.github.com>
2021-11-18 10:40:42 +0200

    added arrow buttons

---
+++ public/profile/profile.js
@@ -0,0 +1,48 @@
+const leftBtn = document.querySelector("#leftbtn")
+const rightBtn = document.querySelector("#rightbtn")
+const profileImg = document.querySelector(".imageIcon")
+const searchBox = document.querySelector("#searchBox");
+
+leftBtn.addEventListener("click", (event) => {
+
+  let urlArray = window.location.href.replace("#", "").split("/");
+  fetch(`/image/${urlArray[urlArray.length - 1]}`, {
+    method: "POST",
+    body: JSON.stringify({ direction: "left" }),
+    headers: { "content-type": "application/json" },
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error(response.statusText).message;
+      return response.json();
+    })
+    .then((res) => {
+      if (res.response != "Successful") return Promise.reject(res.response);
+      profileImg.src = `/icons/${res.data}.png`
+    })
+    .catch(err => {
+      searchBox.value = "Error, can't retrieve image";
+      searchBox.style.color = "#fc6161";
+    });
+})
+
+rightBtn.addEventListener("click", (event) => {
+
+  let urlArray = window.location.href.replace("#", "").split("/");
+  fetch(`/image/${urlArray[urlArray.length - 1]}`, {
+    method: "POST",
+    body: JSON.stringify({ direction: "right" }),
+    headers: { "content-type": "application/json" },
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error(response.statusText).message;
+      return response.json();
+    })
+    .then((res) => {
+      if (res.response != "Successful") return Promise.reject(res.response);
+      profileImg.src = `/icons/${res.data}.png`
+    })
+    .catch(err => {
+
+    });
+})
+
